2b601a9bd2b888c726075781594ada56b0095c84
Lt <2783798559@qq.com>
2021-12-08 17:01:38 -0800

    install express sucessfully

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -2,6 +2,6 @@ const { request } = require("express")
 const express=require("express")
 const app=express()
 app.get("/",(req,res)=>{
-    res.send("hello")
+    res.send("hello world")
 })
 app.listen(10618)
