66fc99f76132754d5ca574847142190b6002b1a2
Lt <2783798559@qq.com>
2021-12-12 17:26:18 -0800

    momgodn first

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const app=express()
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test1');
+mongoose.connect('mongodb://172.21.2.236:27017/190110910618');
 const schema={
     name:String,
     age:Number,
@@ -9,12 +9,15 @@ const schema={
     hooby:String
 }
 const mydata = mongoose.model('cat1s', schema);
-const kitty = new mydata({ name: 'testZildjian' });
-kitty.save()
+// const kitty = new mydata({ name: 'testZildjian' });
+// kitty.save()
 
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
     console.log(req.query)
+    //const kitty = new mydata({ name: req.query.first,health:req.query.second });
+    const kitty = new mydata({ name:'aaaa'});
+    kitty.save()
  })
 app.listen(10618)
